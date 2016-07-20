f5417fd45ce2fcc077ec7c5a8876de26fec44d4d
Toy-godlike <496895884@qq.com>
2016-07-20 14:25:47 +0800

    Deployed Complete!

--- src/main/java/dao/DiscountDAOImpl.java
+++ src/main/java/dao/DiscountDAOImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 /**
--- src/main/java/service/DetectionService.java
+++ src/main/java/service/DetectionService.java
@@ -1,10 +1,12 @@
 package service;
 
 import javax.jws.WebMethod;
+import javax.jws.WebService;
 
 /**
  * Created by Administrator on 2016/7/19.
  */
+@WebService
 public interface DetectionService {
 
     @WebMethod
